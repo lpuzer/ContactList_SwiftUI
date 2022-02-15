685697bfa6ae268afd632069da0e7efd2d21ed8a
lpuzer <lpuzer@yahoo.com.br>
2022-02-14 21:56:57 -0300

    Version 1.0.0 - First ready-to-use version.

--- ContactList_SwiftUI/ContactList_SwiftUI/Utilities/Constants.swift
+++ ContactList_SwiftUI/ContactList_SwiftUI/Utilities/Constants.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 enum Constants {
-    static let baseURL = "https://24df-2804-431-c7f5-c71f-d06b-8069-76a6-5d33.ngrok.io/"
+    static let baseURL = "https://api-vapor-contaclist.herokuapp.com/"
 }
 
 enum Endpoints {
